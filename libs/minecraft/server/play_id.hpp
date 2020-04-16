eee82f35a47ab15b3600f72d483d37ec90543041
Alex Hodges <alexander.hodges11@live.co.uk>
2020-04-16 16:43:08 +0200

    Added new packet id

--- libs/minecraft/server/play_id.hpp
+++ libs/minecraft/server/play_id.hpp
@@ -4,6 +4,6 @@
 
 namespace minecraft::server
 {
-    WISE_ENUM_CLASS((play_id, std::int32_t), (chat_message, 0x0f), (spawn_position, 0x4E));
+    WISE_ENUM_CLASS((play_id, std::int32_t), (chat_message, 0x0f), (spawn_position, 0x4E), (player_position_and_look,0x36));
 
 }
