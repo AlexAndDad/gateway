1b8fcdce221b3e9a32ed444691b9d74d5f7ab8a4
Alex Hodges <alexander.hodges11@live.co.uk>
2020-04-21 20:54:37 +0200

    updating bus

--- src/region/fake_bus.hpp
+++ src/region/fake_bus.hpp
@@ -12,26 +12,40 @@ namespace region
     struct fake_bus
     {
         using executor_type = config::net::io_context::executor_type;
-
+        using strand_type   = net::strand< executor_type >;
 
         fake_bus(executor_type exec)
+        : strand_(net::make_strand(exec))
         {
         }
 
-        net::awaitable<std::string> consume_username()
-        {
+        net::awaitable< const std::string > consume_username() {}
 
+        void produce_username(std::string username)
+        {
+            dispatch(bind_executor(get_strand(), [this, username = std::move(username)] mutable {
+                this->handle_produce_username(std::move(username));
+            }));
         }
 
-        void generate_add_player_func()
-        {}
 
 
       private:
+        strand_type get_strand() { return strand_; }
+
+        void        handle_produce_username(std::string username)
+        {
+            BOOST_ASSERT(get_strand().running_in_this_thread());
 
 
+        }
+        // Net
+        strand_type strand_;
 
-        executor_type strand_;
+        // Username queue
+        std::vector
 
+
+        // Packet queue
     };
 }   // namespace region
