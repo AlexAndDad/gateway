8819d4905aaeeaafb83577a7aa52a5f0f774d564
Richard Hodges <hodges.r@gmail.com>
2020-04-23 01:00:48 +0200

    switching threads

--- libs/config/tests/asio_coroutine_behaviour.spec.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by rhodges on 23/04/2020.
-//
---
+++ libs/polyfill/tests/asio_coroutine_behaviour.spec.cpp
@@ -0,0 +1,104 @@
+//
+// Created by rhodges on 23/04/2020.
+//
+
+#include "polyfill/net.hpp"
+#include "polyfill/net/future.hpp"
+
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <optional>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+
+using namespace polyfill;
+
+TEST_CASE("polyfill::tests::asio_coroutine_behaviour")
+{
+    SECTION("which executor")
+    {
+        struct context
+        {
+            static auto get_map() -> std::vector< std::tuple< std::string, context * > > &
+            {
+                static std::vector< std::tuple< std::string, context * > > vec_;
+                return vec_;
+            }
+
+            context(std::string name)
+            : name(name)
+            {
+            }
+
+            void start()
+            {
+                work_guard.emplace(net::make_work_guard(ioc));
+                thread = std::thread([this] { this->ioc.run(); });
+                get_map().emplace_back(name, this);
+            }
+
+            std::thread                                                                 thread;
+            net::io_context                                                             ioc;
+            std::optional< net::executor_work_guard< net::io_context::executor_type > > work_guard;
+            std::string                                                                 name;
+        };
+
+        std::unordered_map< std::thread::id, std::string > names;
+
+        context a("a");
+        context b("b");
+
+        auto which_thread = [] {
+            auto id   = std::this_thread::get_id();
+            auto iter = std::find_if(context::get_map().begin(), context::get_map().end(), [id](auto &&elem) {
+                auto &[name, pcontext] = elem;
+                return id == pcontext->thread.get_id();
+            });
+            return std::get< std::string >(*iter);
+        };
+
+        a.start();
+        b.start();
+
+        std::promise< void > p;
+        net::co_spawn(
+            a.ioc.get_executor(),
+            [&]() -> net::awaitable< void > {
+                std::cout << "Checkpoint 1 : thread " << which_thread() << std::endl;
+
+                CHECK(which_thread() == "a");
+
+                co_await net::dispatch(b.ioc.get_executor(), net::use_awaitable);
+
+                CHECK(which_thread() == "a");
+                std::cout << "Checkpoint 2 : thread " << which_thread() << std::endl;
+
+                auto ap = net::promise< int >(co_await net::this_coro::executor);
+                net::co_spawn(
+                    b.ioc.get_executor(),
+                    [&]() -> net::awaitable< int > {
+                        CHECK(which_thread() == "b");
+                        std::cout << "Checkpoint 3 : thread " << which_thread() << std::endl;
+                        co_return 0;
+                    },
+                    [&](std::exception_ptr, int x) { ap.set_value(x); });
+
+                auto v = co_await ap.get_future()();
+                CHECK(which_thread() == "a");
+                CHECK(v == 0);
+                std::cout << "Checkpoint 4 : thread " << which_thread() << " with value " << v << std::endl;
+
+                p.set_value();
+                co_return;
+            },
+            net::detached);
+
+        p.get_future().wait();
+
+        a.work_guard.reset();
+        b.work_guard.reset();
+        a.thread.join();
+        b.thread.join();
+    }
+}
