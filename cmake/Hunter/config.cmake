5e353cf4fefe8b50c63497c42b8c913059b085e6
Richard Hodges <hodges.r@gmail.com>
2020-04-22 13:10:58 +0200

    fix

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -14,3 +14,8 @@ hunter_config(BoostWebclient
         GIT_SUBMODULE git-deps/boost-webclient
         CONFIGURATION_TYPES Release RelWithDebInfo Debug
         CMAKE_ARGS BOOST_WEBCLIENT_USE_HUNTER=ON)
+
+hunter_config(BoostJson
+        GIT_SUBMODULE git-deps/boost_json
+        CONFIGURATION_TYPES Release Debug
+        CMAKE_ARGS BOOST_JSON_ALLOW_TESTS=OFF)
