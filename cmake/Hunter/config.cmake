72c1114ccc4f866ba8589dc1bb3ca8b48a9b8414
Richard Hodges <hodges.r@gmail.com>
2020-04-25 13:19:52 +0200

    add boost_webclient

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -19,3 +19,12 @@ hunter_config(boost_json
         GIT_SUBMODULE git-deps/boost_json
         CONFIGURATION_TYPES Release
         CMAKE_ARGS BOOST_JSON_ALLOW_TESTS=NO)
+
+hunter_config(uriparser
+        URL https://github.com/uriparser/uriparser/releases/download/uriparser-0.9.3/uriparser-0.9.3.tar.gz
+        SHA1 5f041e89e90c53717f047ddca01871afc45010a3
+        CMAKE_ARGS
+        BUILD_SHARED_LIBS=OFF
+        URIPARSER_BUILD_DOCS=OFF
+        URIPARSER_BUILD_TESTS=OFF
+        URIPARSER_BUILD_TOOLS=OFF)
