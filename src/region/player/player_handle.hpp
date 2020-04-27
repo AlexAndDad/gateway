6fb380e69c6cabc263ffd772b2071c401bc1091c
Alex Hodges <alexander.hodges11@live.co.uk>
2020-04-27 20:10:11 +0200

    Added player_handle

---
+++ src/region/player/player_handle.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "minecraft/player.hpp"
+
+#include <memory>
+
+namespace region::player
+{
+    template < class PlayerType >
+    struct player_handle
+    {
+        using impl_type = PlayerType;
+
+        player_handle(std::shared_ptr<PlayerType> impl)
+        : impl_(std::move(impl))
+        {
+        }
+
+        impl_type &      get() { return *impl_; }
+        const impl_type &get() const { return *impl_; }
+
+      private:
+        std::shared_ptr< PlayerType > impl_;
+    };
+}   // namespace region::player
