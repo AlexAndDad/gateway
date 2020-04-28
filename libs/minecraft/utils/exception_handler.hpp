b02becd8d0e248952aab3c20a42edc79b778c2f2
Alex Hodges <alexander.hodges11@live.co.uk>
2020-04-28 11:38:00 +0200

    Added new exception handler that doesn't require shared_ptr

--- libs/minecraft/utils/exception_handler.hpp
+++ libs/minecraft/utils/exception_handler.hpp
@@ -42,10 +42,45 @@ namespace minecraft::utils
         std::string             context;
     };
 
+    struct exception_handler_standalone
+    {
+        exception_handler_standalone(std::string &&context)
+        : context(std::move(context))
+        {
+        }
+
+        void operator()(std::exception_ptr ep) const
+        {
+            try
+            {
+                if (ep)
+                    std::rethrow_exception(ep);
+            }
+            catch (system_error &se)
+            {
+                auto &&ec = se.code();
+                if (ec == net::error::operation_aborted)
+                    return;
+                spdlog::error("[{}]({})", context, report(ec));
+            }
+            catch (...)
+            {
+                spdlog::error("[{}] - exception: {}", context, polyfill::explain());
+            }
+        }
+
+        std::string context;
+    };
+
     template < class Self >
     auto make_exception_handler(Self *self, std::string context)
     {
         return exception_handler< Self >(self->shared_from_this(), std::move(context));
+    }
+
+    auto make_exception_handler_standalone(std::string context)
+    {
+        return exception_handler_standalone(std::move(context));
     }
 
 }   // namespace minecraft::utils
