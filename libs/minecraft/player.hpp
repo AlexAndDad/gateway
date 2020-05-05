35a5ead6f4847667d3f988aa9d341d4380caf9d3
Alex Hodges <alexander.hodges11@live.co.uk>
2020-05-05 12:51:58 +0200

    now handles receiving/sending player position packets

--- libs/minecraft/player.hpp
+++ libs/minecraft/player.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "minecraft/math.hpp"
+#include "polyfill/class_logger.hpp"
 
 #include <memory>
 
@@ -15,7 +16,9 @@ namespace minecraft
     overloaded(Ts...) -> overloaded< Ts... >;
 
     template < class Connection >
-    struct player : std::enable_shared_from_this< player< Connection > >
+    struct player
+    : std::enable_shared_from_this< player< Connection > >
+    , polyfill::class_logger
     {
         using name_type     = std::string;
         using executor_type = net::io_context::executor_type;
@@ -24,10 +27,14 @@ namespace minecraft
         using delta_time_type = std::chrono::milliseconds;
 
         player(std::string name, Connection connection, executor_type exec)
-        : name_(std::move(name))
+        : class_logger("player [" + name + "]")
+        , name_(std::move(name))
         , position_()
-        , rotation_()
+        , yaw_(0)
+        , pitch_(0)
         , has_moved_(true)   // Set this to true so that on a players first tick, they will get sent a position update
+        , has_rotated_(true)
+        , on_ground_(true)
         , strand_(exec.context())
         , connection_(std::move(connection))
         {
@@ -42,25 +49,46 @@ namespace minecraft
         }
 
         /// @brief Signal the player object that it's time to 'disconnect' internally
-        void stop() {}
+        void cancel()
+        {
+            net::co_spawn(
+                get_strand(),
+                [self = this->shared_from_this()]() -> net::awaitable< void > { co_await self->handle_cancel(); },
+                net::detached);
+        }
 
         void tick(delta_time_type delta_time)
         {
-            net::dispatch(
-                net::bind_executor(get_strand(), [self = player< Connection >::shared_from_this(), delta_time]() {
+            net::co_spawn(
+                get_strand(),
+                [self = this->shared_from_this(), delta_time]() -> net::awaitable< void > {
                     co_await self->handle_tick(delta_time);
-                }));
+                },
+                net::detached);
         }
 
-      public:
-        net::awaitable< void > handle_tick(delta_time_type delta_time) // Handle logic changes here
+      private:
+        net::awaitable< void > handle_tick(delta_time_type delta_time)   // Handle logic changes here
         {
+            BOOST_ASSERT(get_strand().running_in_this_thread());
             boost::ignore_unused(delta_time);
+
+            if (has_moved_)
+            {
+                auto pack_wrapper = minecraft::packets::server::server_play_packet();
+                auto packet       = minecraft::packets::server::player_position_and_look();
+                packet.x          = position_.x();
+                packet.y          = position_.y();
+                packet.z          = position_.z();
+                pack_wrapper.set(std::move(packet));
+            }
+
             co_return;
         }
 
         net::awaitable< void > run()
         {
+            BOOST_ASSERT(get_strand().running_in_this_thread());
             // TODO Do start logic to setup the player
 
             // Handle packets
@@ -70,39 +98,75 @@ namespace minecraft
                 {
                     auto packet = co_await connection_.consume_packet();
 
-                    visit(overloaded { [](monostate &arg) {
+                    visit(overloaded { [this](monostate &arg) {
                                           boost::ignore_unused(arg);
-                                          spdlog::warn("got monostate while consuming a packet in player.");
+                                          this->warn("got monostate while consuming a packet in player.");
                                       },
-                                       [](auto &arg) {
+                                       [this](auto &arg) {
                                            boost::ignore_unused(arg);
-                                           spdlog::warn("unhandled packet: {} consumed in player. Ignoring...",
-                                                        wise_enum::to_string(arg.id()));
+                                           this->warn("unhandled packet: {} consumed in player. Ignoring...",
+                                                      wise_enum::to_string(arg.id()));
                                        },
-                                       [](minecraft::packets::client::animation &arg) {
-                                           spdlog::warn("unhandled animation packet");
+                                       [this](minecraft::packets::client::animation &arg) {
+                                           this->warn("unhandled animation packet");
                                            boost::ignore_unused(arg);
+                                       },
+                                       [this](minecraft::packets::client::player_position_and_rotation &arg) {
+                                           player_position_and_rotation_handler(arg);
                                        } },
                           packet.as_variant());
                 }
                 catch (std::exception &)
                 {
-                    spdlog::error("{}", polyfill::explain());
+                    error("{}", polyfill::explain());
                     break;
                 }
             }
         }
 
-      private:
+        net::awaitable< void > handle_cancel()
+        {
+            BOOST_ASSERT(get_strand().running_in_this_thread());
+            // Cancel everything
+            connection_.cancel();
+            co_return;
+        }
+
+      private:   // Packet handlers
+        void player_position_and_rotation_handler(const minecraft::packets::client::player_position_and_rotation &arg)
+        {
+            auto new_pos = math::Vector3d(arg.x, arg.feet_y, arg.z);
+            if (new_pos != position_)
+            {
+                position_  = std::move(new_pos);
+                has_moved_ = true;
+            }
+            auto set_rot = [this](float &dest, float val) mutable {
+                if (val != dest)
+                {
+                    dest         = val;
+                    has_rotated_ = true;
+                }
+            };
+
+            set_rot(yaw_, arg.yaw);
+            set_rot(pitch_, arg.pitch);
+        }
+
+      private:   // data
         strand_type &get_strand() { return strand_; }
 
         name_type name_;
 
         // World related data
-        math::Vector3d position_;   // XZ center, Y bottom
-        math::Vector3d rotation_;   // degrees [-180, +180]
+        math::Vector3d position_;   // XZ center, Y feet (bottom)
 
-        bool has_moved_;   // has the player moved since the last position packet was sent
+        float yaw_;
+        float pitch_;
+
+        bool has_moved_;     // has the player moved
+        bool has_rotated_;   // has the player rotated
+        bool on_ground_;     // this variable represents the clients view of what the player is standing on.
 
         strand_type strand_;
         Connection  connection_;
