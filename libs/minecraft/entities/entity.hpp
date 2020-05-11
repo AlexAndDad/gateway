0ad2c0b4b1b20aee3a6395caa3b9fc0540d799b2
Alex Hodges <alexander.hodges11@live.co.uk>
2020-05-11 18:41:17 +0200

    Added entitys

---
+++ libs/minecraft/entities/entity.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "entities/entity_traits.hpp"
+
+namespace minecraft::entities
+{
+
+    struct entity: entity_traits
+    {
+        entity(entity_id_type id)
+        {}
+    };
+}
---
+++ libs/minecraft/entities/entity_traits.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace minecraft::entities
+{
+    struct entity_traits
+    {
+        using entity_id_type = std::uint32_t;
+    };
+
+}
