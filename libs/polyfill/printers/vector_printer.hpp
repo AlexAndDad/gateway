1666dfe2a1f31d058713c204977ce997f6d52321
Richard Hodges <hodges.r@gmail.com>
2020-05-14 12:05:37 +0200

    fix: vector_printer transform

--- libs/polyfill/printers/vector_printer.hpp
+++ libs/polyfill/printers/vector_printer.hpp
@@ -1,38 +1,46 @@
 #pragma once
 
+#include <utility>
 #include <vector>
 
 namespace polyfill::printers
 {
-    template < class T >
+    struct identity
+    {
+        template < class Arg >
+        Arg &&operator()(Arg &&arg) const
+        {
+            return std::forward< Arg >(arg);
+        }
+    };
+
+    template < class T, class Transform >
     struct vector_printer
     {
-        vector_printer(const std::vector< T > &vector)
-        :vector(vector)
-        {}
+        vector_printer(const std::vector< T > &vector, Transform transform)
+        : vector(vector)
+        , transform_(std::move(transform))
+        {
+        }
 
         template < class Stream >
-        friend Stream &operator<<(Stream &stream, const vector_printer< T > &self)
+        friend Stream &operator<<(Stream &stream, const vector_printer< T, Transform > &self)
         {
             auto number = 0;
             for (auto &item : self.vector)
-                stream << number++ << ": " << item << "\n";
+                stream << number++ << ": " << self.transform_(item) << "\n";
             return stream;
         }
 
       private:
         const std::vector< T > &vector;
+        Transform               transform_;
     };
 
-    template < class T >
-    vector_printer< T > print(const std::vector< T > &vector)
+    template < class T, class Transform = identity >
+    vector_printer< T, Transform > print(const std::vector< T > &vector, Transform transform = Transform())
     {
-        return vector_printer< T >(vector, [](auto && arg){return arg;});
+        return vector_printer< T, Transform >(vector, std::move(transform));
     }
 
-    template < class T , class F>
-    vector_printer< T > print(const std::vector< T > &vector, F transform_func)
-    {
-        return vector_printer< T >(vector,std::move(transform_func));
-    }
 }   // namespace polyfill::printers
