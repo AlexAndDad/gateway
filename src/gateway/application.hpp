2b6c8e8ccd8d2a34855276991defd6c40d8cd219
Alex Hodges <alexander.hodges11@live.co.uk>
2020-05-24 09:11:13 +0200

    refactoring

--- src/gateway/application.hpp
+++ src/gateway/application.hpp
@@ -1,74 +1,65 @@
 #pragma once
 #include "config/net.hpp"
+
+
 #include "listener.hpp"
+#include "player_manager.hpp"
+
+
+#include "minecraft/region/player_updates_queue.hpp"
 #include "minecraft/security/private_key.hpp"
 #include "minecraft/security/rsa.hpp"
-#include "minecraft/region/player_updates_queue.hpp"
+#include "polyfill/async/coro_except_handler.hpp"
+#include "polyfill/configuration.hpp"
 
 namespace gateway
 {
-    struct app_config : listener_config
-    {
-        auto as_listener_config() const -> listener_config const&
-        {
-            return *this;
-        }
-
-        friend auto operator<<(std::ostream& os, app_config const& cfg) -> std::ostream&
-        {
-            os << "Application Config\n";
-            os << cfg.as_listener_config();
-            return os;
-        }
-
-    };
-
     struct application
     {
         using executor_type = net::io_context::executor_type;
-        using signal_set = net::basic_signal_set<executor_type>;
+        using signal_set    = net::basic_signal_set< executor_type >;
 
-        application(executor_type exec, minecraft::region::player_update_queue & queue)
-        : signals_(exec)
-        , listener_(exec, config_, queue)
+        application(executor_type exec, polyfill::configuration config)
+        : config_(std::move(config))
+        , signals_(exec)
+        , listener_(exec, config_)
+        , player_manager_(exec,config_)
         {
-            std::cout << "Application Starting\n\n";
-            std::cout << config_ << std::endl;
             signals_.add(SIGINT);
         }
 
         void start()
         {
-            std::cout << "Application start called" << std::endl;
-            dispatch(bind_executor(get_executor(), [this]{
-                this->handle_start();
-            }));
+            net::co_spawn(
+                get_executor(),
+                [this]() mutable -> net::awaitable< void > {
+                    co_await this->run();
+                },
+                polyfill::coro_except_handler("application", "start()"));
         }
 
         void cancel()
         {
-            dispatch(bind_executor(get_executor(), [this]{
-                this->handle_cancel();
-            }));
+            dispatch(bind_executor(get_executor(),
+                                   [this] { this->handle_cancel(); }));
         }
 
-        auto get_executor() -> executor_type
-        {
-            return signals_.get_executor();
-        }
+        auto get_executor() -> executor_type { return signals_.get_executor(); }
 
-    private:
-
-        void handle_start()
+      private:
+        net::awaitable< void > run()
         {
-            std::cout << "Application handle_start called" << std::endl;
-            signals_.async_wait([this](error_code const& ec, int sig){
+            // Setup signals callback
+            signals_.async_wait([this](error_code const &ec, int sig) {
                 handle_signal(ec, sig);
             });
-            start_all_services();
+
+            // Start services
+            co_await listener_.start();
+            co_await player_manager_.start();
         }
 
-        void handle_signal(error_code const& ec, int sig)
+        void handle_signal(error_code const &ec, int sig)
         {
             if (ec.failed())
             {
@@ -95,19 +86,12 @@ namespace gateway
             cancel_all_services();
         }
 
-        void start_all_services()
-        {
-            listener_.start();
-        }
+        void cancel_all_services() { listener_.cancel(); }
 
-        void cancel_all_services()
-        {
-            listener_.cancel();
-        }
+        polyfill::configuration config_;
 
-        app_config config_;
-
-        signal_set signals_;
-        listener listener_;
+        signal_set     signals_;
+        listener       listener_;
+        player_manager player_manager_;
     };
-}
+}   // namespace gateway
