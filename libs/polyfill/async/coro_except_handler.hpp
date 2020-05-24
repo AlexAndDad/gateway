43c97e3f8561ce57a2bebee008774e6e8aaafe68
Alex Hodges <alexander.hodges11@live.co.uk>
2020-05-24 08:50:03 +0200

    Added to polyfill

---
+++ libs/polyfill/async/coro_except_handler.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "explain.hpp"
+
+#include <exception>
+#include <string_view>
+
+namespace polyfill
+{
+    struct coro_except_handler
+    {
+        coro_except_handler(const std::string_view struct_name,
+                            const std::string_view function_name)
+        : struct_name_(std::move(struct_name))
+        , function_name_(std::move(function_name))
+        {
+        }
+
+        void operator()(std::exception_ptr ep)
+        {
+            if (ep)
+            {
+                spdlog::error("{}::{} - exception: ",
+                              struct_name_,
+                              function_name_,
+                              polyfill::explain());
+                std::rethrow_exception(ep);
+            }
+        }
+
+      private:
+        const std::string_view struct_name_;
+        const std::string_view function_name_;
+    };
+}   // namespace polyfill
