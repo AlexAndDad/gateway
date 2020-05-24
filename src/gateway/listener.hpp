0a38e079ff7405e311281913ad46287b09a55560
Alex Hodges <alexander.hodges11@live.co.uk>
2020-05-24 08:51:56 +0200

    Listener no longer handles creating connections, it now just listens for new sockets.

--- src/gateway/listener.hpp
+++ src/gateway/listener.hpp
@@ -3,6 +3,8 @@
 #include "config/net.hpp"
 #include "connection_cache.hpp"
 #include "minecraft/security/private_key.hpp"
+#include "polyfill/async/async_task.hpp"
+#include "polyfill/configuration.hpp"
 
 #include <iostream>
 
@@ -10,36 +12,66 @@ using namespace std::literals;
 
 namespace gateway
 {
-    struct listener_config : connection_config
-    {
-        auto as_connection_config() const -> connection_config const & { return *this; }
-
-        std::string listen_port = "25565";
-
-        friend auto operator<<(std::ostream &os, listener_config const &cfg) -> std::ostream &;
-    };
-
     struct listener
     {
         using executor_type = net::io_context::executor_type;
 
-        using protocol      = net::ip::tcp;
-        using socket_type   = net::basic_stream_socket< protocol, executor_type >;
-        using acceptor_type = net::basic_socket_acceptor< protocol, executor_type >;
+        using protocol    = net::ip::tcp;
+        using socket_type = net::basic_stream_socket< protocol, executor_type >;
+        using acceptor_type =
+            net::basic_socket_acceptor< protocol, executor_type >;
 
-        listener(executor_type exec, listener_config config, minecraft::region::player_update_queue &queue)
-        : config_(std::move(config))
+        listener(executor_type exec, polyfill::configuration &config)
+        : config_(config)
         , acceptor_(exec)
-        , queue_(queue)
+        , port_(config_["listener"].as_object()["port"].as_uint64())
+        {
+        }
+
+        net::awaitable< void > start()
+        {
+            co_await polyfill::async::async_task(
+                get_executor(),
+                [this]() mutable -> net::awaitable< void > {
+                    co_await handle_start();
+                },
+                "listener",
+                "start()");
+        }
+
+        void cancel()
+        {
+            dispatch(bind_executor(get_executor(),
+                                   [this] { this->handle_cancel(); }));
+        }
+
+        net::awaitable< socket_type > listen()
+        {
+            co_return co_await polyfill::async::async_task(
+                get_executor(),
+                [this]() mutable -> net::awaitable< socket_type > {
+                    co_return co_await handle_listen();
+                },
+                "listener",
+                "listen()");
+        }
+
+        auto get_executor() -> net::executor
+        {
+            return acceptor_.get_executor();
+        }
+
+      private:
+        net::awaitable< void > handle_start()
         {
             acceptor_.open(protocol::v4());
             acceptor_.set_option(socket_type::reuse_address(true));
             auto ec = error_code();
             do
             {
-                acceptor_.bind(protocol::endpoint(net::ip::make_address("0.0.0.0"),
-                                                  std::uint16_t(::atoi(config_.listen_port.c_str()))),
-                               ec);
+                acceptor_.bind(
+                    protocol::endpoint(net::ip::make_address("0.0.0.0"), port_),
+                    ec);
                 if (not ec.failed())
                     break;
                 if (ec.failed() && ec != net::error::address_in_use)
@@ -51,30 +83,44 @@ namespace gateway
             } while (ec.failed());
 
             acceptor_.listen();
+            co_return;
         }
 
-        void start()
+        net::awaitable< socket_type > handle_listen()
         {
-            dispatch(bind_executor(get_executor(), [this] { this->initiate_accept(); }));
+            for (;;)
+            {
+                try
+                {
+                    co_return co_await acceptor_.async_accept(
+                        net::use_awaitable);
+                }
+                catch (system_error &se)
+                {
+                    auto ec = se.code();
+                    if (ec == net::error::connection_aborted)
+                        continue;
+                    else if (ec != net::error::operation_aborted)
+                        std::rethrow_exception(std::current_exception());
+                }
+                catch (...)
+                {
+                    std::rethrow_exception(std::current_exception());
+                }
+            }
         }
 
-        void cancel()
+        void handle_cancel()
         {
-            dispatch(bind_executor(get_executor(), [this] { this->handle_cancel(); }));
+            acceptor_.cancel();
+            connections_.cancel();
         }
+        polyfill::configuration &config_;
 
-        auto get_executor() -> net::executor { return acceptor_.get_executor(); }
+        acceptor_type    acceptor_;
+        connection_cache connections_;
 
-      private:
-        void initiate_accept();
-
-        void handle_accept(error_code ec, socket_type sock);
-
-        void handle_cancel();
-
-        listener_config                         config_;
-        acceptor_type                           acceptor_;
-        connection_cache                        connections_;
-        minecraft::region::player_update_queue &queue_;
+        std::uint16_t port_;
     };
+
 }   // namespace gateway
